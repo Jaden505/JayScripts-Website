52d84e70cf2eb862cf3cd1486cc38f1680ce44f7
Jaden van Rijswijk <jaden.van.rijswijk@hva.nl>
2022-04-14 18:54:03 +0200

    Remove cached repo

--- flaskr/main.py
+++ flaskr/main.py
@@ -6,18 +6,3 @@ main = Blueprint('main', __name__)
 def home():
     return render_template('home.html')
 
-# @main.route('/about')
-# def about():
-#     return render_template('about.html')
-
-# @main.route('/contact')
-# def contact():
-#     return render_template('contact.html')
-
-# @main.route('/projects')
-# def projects():
-#     return render_template('projects.html')
-
-# @main.route('/portfolio')
-# def portfolio():
-#     return render_template('portfolio.html')
